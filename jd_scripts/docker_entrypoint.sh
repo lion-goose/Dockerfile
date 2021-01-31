f2b736af5d810753d878d8812f40632cb83c9f5a
Angeliss <59443350+lion-goose@users.noreply.github.com>
2021-01-31 11:51:57 +0800

    Create docker_entrypoint.sh

---
+++ jd_scripts/docker_entrypoint.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+set -e
+
+#获取配置的自定义参数
+if [ $1 ]; then
+    run_cmd=$1
+fi
+
+echo "设定远程仓库地址..."
+cd /scripts
+git remote set-url origin $REPO_URL
+git reset --hard
+echo "git pull拉取最新代码..."
+git -C /scripts pull --rebase
+echo "npm install 安装最新依赖"
+npm install --prefix /scripts
+
+#任务脚本shell仓库
+cd /jds
+git pull origin master --rebase
+
+echo "------------------------------------------------执行定时任务任务shell脚本------------------------------------------------"
+sh /jds/jd_scripts/task_shell_script.sh
+echo "--------------------------------------------------默认定时任务执行完成---------------------------------------------------"
+
+if [ $run_cmd ]; then
+    echo "Start crontab task main process..."
+    echo "启动crondtab定时任务主进程..."
+    crond -f
+else
+    echo "默认定时任务执行结束。"
+fi
