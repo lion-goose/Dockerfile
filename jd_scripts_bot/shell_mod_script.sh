d254f9ab1b319ca6e522f2cd155e931f27910f81
Angeliss <59443350+lion-goose@users.noreply.github.com>
2021-04-12 15:17:18 +0800

    Update shell_mod_script.sh

--- jd_scripts_bot/shell_mod_script.sh
+++ jd_scripts_bot/shell_mod_script.sh
@@ -28,7 +28,11 @@ cp -f /lion-goose/jd*.js /scripts/
 #### monk-coder https://github.com/monk-coder/dust
 function monkcoder(){
     # https://github.com/monk-coder/dust
+    echo "未检查到lion-goose仓库脚本，初始化下载相关脚本"
     [[ ! -d /monkcoder ]] && git clone https://github.com/sensi-ribbed/temple.git /monkcoder
+    echo "更新monkcoder脚本相关文件"
+    git -C /monkcoder reset --hard
+    git -C /monkcoder pull --rebase
     # 拷贝脚本
     for jsname in $(find /monkcoder -name "*.js" | grep -vE "\/backup\/"); do cp ${jsname} /scripts/monkcoder_${jsname##*/}; done
     # 匹配js脚本中的cron设置定时任务
