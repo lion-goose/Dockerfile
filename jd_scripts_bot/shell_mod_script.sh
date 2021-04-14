11703ad7e2802dd0eb80e727654bcb755a7d60e5
Angeliss <59443350+lion-goose@users.noreply.github.com>
2021-04-14 19:33:31 +0800

    Update shell_mod_script.sh

--- jd_scripts_bot/shell_mod_script.sh
+++ jd_scripts_bot/shell_mod_script.sh
@@ -25,17 +25,44 @@ fi
 ##复制两个文件
 cp -f /lion-goose/jd*.js /scripts/
 
-#### monk-coder https://github.com/monk-coder/dust
+# #### monk-coder https://github.com/monk-coder/dust
+# function monkcoder(){
+#     # https://github.com/monk-coder/dust
+# #     rm -rf /monkcoder /scripts/monkcoder_*
+# #     git clone https://github.com/sensi-ribbed/temple.git /monkcoder
+#     # 拷贝脚本
+#     for jsname in $(find /monkcoder -name "*.js" | grep -vE "\/backup\/"); do cp ${jsname} /scripts/monkcoder_${jsname##*/}; done
+#     # 匹配js脚本中的cron设置定时任务
+#     for jsname in $(find /monkcoder -name "*.js" | grep -vE "\/backup\/"); do
+#         jsnamecron="$(cat $jsname | grep -oE "/?/?cron \".*\"" | cut -d\" -f2)"
+#         test -z "$jsnamecron" || echo "$jsnamecron node /scripts/monkcoder_${jsname##*/} >> /scripts/logs/monkcoder_${jsname##*/}.log 2>&1" >> /scripts/docker/merged_list_file.sh
+#     done
+# }
+
+
 function monkcoder(){
-    # https://github.com/monk-coder/dust
-#     rm -rf /monkcoder /scripts/monkcoder_*
-#     git clone https://github.com/sensi-ribbed/temple.git /monkcoder
-    # 拷贝脚本
-    for jsname in $(find /monkcoder -name "*.js" | grep -vE "\/backup\/"); do cp ${jsname} /scripts/monkcoder_${jsname##*/}; done
-    # 匹配js脚本中的cron设置定时任务
-    for jsname in $(find /monkcoder -name "*.js" | grep -vE "\/backup\/"); do
-        jsnamecron="$(cat $jsname | grep -oE "/?/?cron \".*\"" | cut -d\" -f2)"
-        test -z "$jsnamecron" || echo "$jsnamecron node /scripts/monkcoder_${jsname##*/} >> /scripts/logs/monkcoder_${jsname##*/}.log 2>&1" >> /scripts/docker/merged_list_file.sh
+    # https://share.r2ray.com/dust/
+    apk add --no-cache --upgrade grep
+    i=1
+    while [ "$i" -le 5 ]; do
+        folders="$(curl -sX POST "https://share.r2ray.com/dust/" | grep -oP "name.*?\.folder" | cut -d, -f1 | cut -d\" -f3 | grep -vE "backup|pics|rewrite" | tr "\n" " ")"
+        test -n "$folders" && { rm -rf /scripts/dust_*; break; } || { echo 第 $i/5 次目录列表获取失败; i=$(( i + 1 ));}
+    done
+    for folder in $folders; do
+        i=1
+        while [ "$i" -le 5 ]; do
+            jsnames="$(curl -sX POST "https://share.r2ray.com/dust/${folder}/" | grep -oP "name.*?\.js\"" | grep -oE "[^\"]*\.js\"" | cut -d\" -f1 | tr "\n" " ")"
+            test -n "$jsnames" && break || { echo 第 $i/5 次 $folder 目录下文件列表获取失败; i=$(( i + 1 )); }
+        done
+        for jsname in $jsnames; do 
+            i=1
+            while [ "$i" -le 5 ]; do
+                curl -so /scripts/dust_${jsname} "https://share.r2ray.com/dust/${folder}/${jsname}"
+                jsnamecron="$(cat /scripts/dust_$jsname | grep -oE "/?/?cron \".*\"" | cut -d\" -f2)"
+                test -n "$jsnamecron" && echo "$jsnamecron node /scripts/dust_$jsname >> /scripts/logs/dust_$jsname.log 2>&1" >> /scripts/docker/merged_list_file.sh
+                test "$(wc -c <"/scripts/dust_${jsname}")" -ge 1000 && break || { echo 第 $i/5 次 $folder 目录下 $jsname 文件下载失败; i=$(( i + 1 )); }
+            done
+        done
     done
 }
 
