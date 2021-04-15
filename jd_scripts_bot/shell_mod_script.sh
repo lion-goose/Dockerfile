8b9d8d1af2dccd3455de4ad6dd88576e866738c1
Angeliss <59443350+lion-goose@users.noreply.github.com>
2021-04-15 16:45:04 +0800

    Update shell_mod_script.sh

--- jd_scripts_bot/shell_mod_script.sh
+++ jd_scripts_bot/shell_mod_script.sh
@@ -69,7 +69,7 @@ function monkcoder(){
 
 function diycron(){
     # monkcoder whyour 定时任务
-    for jsname in /scripts/dust_*.js /scripts/whyour_*.js; do
+    for jsname in /scripts/dust_*.js; do
         jsnamecron="$(cat $jsname | grep -oE "/?/?cron \".*\"" | cut -d\" -f2)"
         test -z "$jsnamecron" || echo "$jsnamecron node $jsname >> /scripts/logs/$(echo $jsname | cut -d/ -f3).log 2>&1" >> /scripts/docker/merged_list_file.sh
     done
