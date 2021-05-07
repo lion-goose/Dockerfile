8717ea17a13d728cdba59894ac192c60c7d3594f
Angeliss <59443350+lion-goose@users.noreply.github.com>
2021-05-07 11:13:08 +0800

    Create crontab_list.sh

---
+++ pack_scripts/crontab_list.sh
@@ -0,0 +1,5 @@
+# 每2天的23:50分清理一次日志
+58 23 */1 * * rm -rf /logs/*.log
+
+0 */3 * * * cd /scriptPath/ && python3 keepAlive.py  |ts >> /logs/jd_daily_lottery.log 2>&1
+5 0 * * * cd /scriptPath/ && python3 main.py  |ts >> /logs/jd_daily_lottery.log 2>&1
