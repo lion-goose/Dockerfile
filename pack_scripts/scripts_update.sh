b2fdf1d7f3b77b9be8a92bc262420e94290d1620
Angeliss <59443350+lion-goose@users.noreply.github.com>
2021-05-07 11:15:31 +0800

    Update scripts_update.sh

--- pack_scripts/scripts_update.sh
+++ pack_scripts/scripts_update.sh
@@ -3,7 +3,10 @@ set -e
 
 #sunert 仓库的百度极速版
 if [ -d "/get_CCB" ]; then
-    echo "git pull拉取最新代码..."
+    echo "Pull the get_CCB latest code..."
+    echo "git 拉取get_CCB最新代码..."
+    git -C /get_CCB reset --hard
+    git -C /get_CCB pull
     cd /get_CCB
-    git -C /get_CCB --rebase
+    pip3 install -r requirements.txt
 fi
