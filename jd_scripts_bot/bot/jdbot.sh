259a04d88d61833e7a2ba1925597e4c92ff04188
Angeliss <59443350+lion-goose@users.noreply.github.com>
2021-05-09 19:58:46 +0800

    Update jdbot.sh

--- jd_scripts_bot/bot/jdbot.sh
+++ jd_scripts_bot/bot/jdbot.sh
@@ -7,9 +7,7 @@ BASE_DIR='/scripts'
 # bot更新了新功能的话只需要重启容器就完成更新
 function initBotPythonEnv() {
   echo "开始安装运行jdbot需要的python环境及依赖..."
-  # py3-multidict py3-yarl 为aiogram需要依赖的pip，但是alpine配置gcc编译环境才能安装这两个包，有点浪费，所以直接使用alpine提供的版本
-  # 注释一下省的自己忘了为什么 #gcc musl-dev 为
-  apk add --update python3-dev py3-pip py3-pillow py3-numpy py3-multidict py3-yarl py3-cryptography gcc musl-dev
+  apk add --update python3-dev py3-pip py3-cryptography py3-numpy py-pillow
   echo "开始安装jdbot依赖..."
   cd "$JDS_DIR/bot"
   pip3 install --upgrade pip
@@ -20,12 +18,6 @@ function initBotPythonEnv() {
 function start() {
   if type python3 >/dev/null 2>&1; then
     echo "jdbot所需环境已经存在，跳过安装依赖环境"
-    if [ "$(pip3 list | grep yarl)" == "" ]; then
-      cd "$BASE_DIR"
-      apk add py3-multidict py3-yarl py3-cryptography gcc musl-dev py3-pillow py3-numpy
-      pip3 install --upgrade pip
-      pip3 install -r requirements.txt
-    fi
   else
     echo "jdbot所需环境不存在，初始化所需python3及依赖环境"
     initBotPythonEnv
@@ -45,30 +37,5 @@ function start() {
   fi
 }
 
-function stop() {
-  AP=$(ps -ef | grep "jdbot.py" | grep -v "grep" | awk '{print $1}')
-  if [ -n "$AP" ]; then
-    echo "停止jd bot..."
-    kill -9 $(ps -ef | grep "jdbot.py" | grep -v "grep" | awk '{print $1}')
-  fi
-  echo "进程已停止..."
-}
-function restart() {
-  echo "准备重启..."
-  stop
-  start
-  echo "重启已完成..."
-}
-function restart_bot() {
-  echo "准备重启自己..."
-  stop
-  start
-  echo "重启自己完成..."
-  curl -sX POST "https://api.telegram.org/bot$TG_BOT_TOKEN/sendMessage" -d "chat_id=$TG_USER_ID&text=jd bot重启已完成..." >>/dev/null
-}
-if [ $1 ]; then
-  opt=$1
-  $opt
-else
-  start
-fi
+start
+
