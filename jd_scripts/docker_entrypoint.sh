27b9633bca620a4111bc152060a6df5d78d955f4
Angeliss <59443350+lion-goose@users.noreply.github.com>
2021-05-09 15:19:21 +0800

    Update docker_entrypoint.sh

--- jd_scripts/docker_entrypoint.sh
+++ jd_scripts/docker_entrypoint.sh
@@ -5,15 +5,28 @@ set -e
 if [ $1 ]; then
     run_cmd=$1
 fi
-
-echo "设定远程仓库地址..."
-cd /scripts
-git remote set-url origin $REPO_URL
-git reset --hard
-echo "git pull拉取最新代码..."
-git -C /scripts pull --rebase
-echo "npm install 安装最新依赖"
-npm install --prefix /scripts
+[ -f /scripts/package.json ] && before_package_json=$(cat /scripts/package.json)
+if [ -f "/scripts/logs/pull.lock" ]; then
+  echo "存在更新锁定文件，跳过git pull操作..."
+else
+  echo "设定远程仓库地址..."
+  cd /scripts
+  git remote set-url origin "$REPO_URL"
+  git reset --hard
+  echo "git pull拉取最新代码..."
+  git -C /scripts pull --rebase
+  if [ ! -d /scripts/node_modules ]; then
+    echo "容器首次启动，执行npm install..."
+    npm install --loglevel error --prefix /scripts
+  else
+    if [[ "${before_package_json}" != "$(cat /scripts/package.json)" ]]; then
+      echo "package.json有更新，执行npm install..."
+      npm install --loglevel error --prefix /scripts
+    else
+      echo "package.json无变化，跳过npm install..."
+    fi
+  fi
+fi
 
 #任务脚本shell仓库
 cd /jds
