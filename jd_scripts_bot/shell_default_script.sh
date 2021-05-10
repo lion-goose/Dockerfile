afa04d025e931d188c1bdf1304c369d57b751f97
Angeliss <59443350+lion-goose@users.noreply.github.com>
2021-05-11 07:45:00 +0800

    Update shell_default_script.sh

--- jd_scripts_bot/shell_default_script.sh
+++ jd_scripts_bot/shell_default_script.sh
@@ -18,8 +18,8 @@ elif [ -n "\$(echo \$first | sed -n "/^[0-9]\+\$/p")" ]; then
     echo "\$(echo \$first | sed -n "/^[0-9]\+\$/p")"
     { export JD_COOKIE=\$(sed -n "\${first}p" $COOKIE_LIST) && node \${cmd/\$1/}
     }&
-elif [ -n "\$(cat \$COOKIE_LIST  | grep "pt_pin=\$first")" ];then
-    echo "\$(cat \$COOKIE_LIST  | grep "pt_pin=\$first")"
+elif [ -n "\$(cat $COOKIE_LIST  | grep "pt_pin=\$first")" ];then
+    echo "\$(cat $COOKIE_LIST  | grep "pt_pin=\$first")"
     { export JD_COOKIE=\$(cat $COOKIE_LIST | grep "pt_pin=\$first") && node \${cmd/\$1/}
     }&
 else
