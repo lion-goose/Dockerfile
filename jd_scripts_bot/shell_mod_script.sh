0bc0e7d40153003a739648877a07e19b862e72cb
Angeliss <59443350+lion-goose@users.noreply.github.com>
2021-05-15 11:36:01 +0800

    Update shell_mod_script.sh

--- jd_scripts_bot/shell_mod_script.sh
+++ jd_scripts_bot/shell_mod_script.sh
@@ -76,12 +76,11 @@ function diycron(){
         test -z "$jsnamecron" || echo "$jsnamecron node /scripts/monkcoder_${jsname##*/} >> /scripts/logs/monkcoder_${jsname##*/}.log 2>&1" >> /scripts/docker/merged_list_file.sh
     done
     # JDDJ 定时任务
-    for jsname in /scripts/jddj/*.js; do
-        if [ -n "$(sed -n "s/.*cronexpr=\"\(.*\)\".*/\1/p" $jsname)" ]; then
-            echo "#京东到家仓库任务-$(sed -n "s/.*new Env('\(.*\)').*/\1/p" $jsname)($jsname)" >>$mergedListFile
-            echo "$(sed -n "s/.*cronexpr=\"\(.*\)\".*/\1/p" $jsname) node /scripts/$jsname |ts >>/scripts/logs/$(echo $jsname | sed "s/.js/.log/g") 2>&1 &" >>$mergedListFile
-        fi
+    for jsname in $(ls /scripts/jddj | grep -E "js$" | tr "\n" " "); do
+        jsnamecron="$(cat /scripts/jddj/$jsname | grep -oE "/?/?cron \".*\"" | cut -d\" -f2)"
+        test -z "$jsnamecron" || echo "$jsnamecron node /scripts/jddj/$jsname >> /scripts/logs/jddj_$jsname.log 2>&1" >> /scripts/docker/merged_list_file.sh
     done
+    echo "10 0,8,11,17 * * * node /scripts/jddj/jddj_fruit.js |ts >> /scripts/logs/jddj_fruit.log 2>&1" >> /scripts/docker/merged_list_file.sh
     #### yangtingxiao https://github.com/yangtingxiao/QuantumultX
     wget --no-check-certificate -O /scripts/jd_lottery_machine.js https://raw.githubusercontent.com/yangtingxiao/QuantumultX/master/scripts/jd/jd_lotteryMachine.js
     echo "1 0,16,22 * * * node /scripts/jd_lottery_machine.js |ts >> /scripts/logs/jd_lottery_machine.log 2>&1" >> /scripts/docker/merged_list_file.sh
