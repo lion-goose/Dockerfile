a61200318808bd4be44e3e05b4be6803128373fb
Angeliss <59443350+lion-goose@users.noreply.github.com>
2021-05-21 09:16:22 +0800

    Update shell_mod_script.sh

--- jd_scripts/shell_mod_script.sh
+++ jd_scripts/shell_mod_script.sh
@@ -33,8 +33,8 @@ function main(){
         echo "get_CCB所需环境不存在，初始化所需python3及依赖环境"
         initCcbPythonEnv
     fi
-    echo "48 */3 * * * cd /get_CCB/ && python3 keepAlive.py |ts >> /scripts/logs/ccbkeepAlive.log 2>&1" >> /scripts/docker/merged_list_file.sh
-    echo "12 9,15,21 * * * cd /get_CCB/ && python3 main.py |ts >> /scripts/logs/ccbmain.log 2>&1" >> /scripts/docker/merged_list_file.sh
+    #echo "48 */3 * * * cd /get_CCB/ && python3 keepAlive.py |ts >> /scripts/logs/ccbkeepAlive.log 2>&1" >> /scripts/docker/merged_list_file.sh
+    #echo "12 9,15,21 * * * cd /get_CCB/ && python3 main.py |ts >> /scripts/logs/ccbmain.log 2>&1" >> /scripts/docker/merged_list_file.sh
 }
 main
 
