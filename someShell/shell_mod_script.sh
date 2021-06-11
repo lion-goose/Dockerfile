c2dda80c03a68e00b7ca887ada5d1b74a429f161
Angeliss <59443350+lion-goose@users.noreply.github.com>
2021-06-11 10:38:38 +0800

    Update shell_mod_script.sh

--- someShell/shell_mod_script.sh
+++ someShell/shell_mod_script.sh
@@ -12,6 +12,6 @@ if [ ! -d "/scripts/somescripts/" ]; then
     echo "# 百度和中青任务" >> $mergedListFile
     echo "*/30 5-23 * * * node /scripts/somescripts/youth/youth.js >> /scripts/logs/youth.log 2>&1" >> $mergedListFile
     echo "15 5,8,11,14,17,20 * * * node /scripts/somescripts/youth/Youth_Read-ange.js >> /scripts/logs/Youth_Read-ange.log 2>&1" >> $mergedListFile
-    echo "45 6,9,12,15,18,21 * * * node /scripts/somescripts/Youth_Read-hexor.js >> /scripts/logs/Youth_Read-hexor.log 2>&1" >> $mergedListFile
+    echo "45 6,9,12,15,18,21 * * * node /scripts/somescripts/youth/Youth_Read-hexor.js >> /scripts/logs/Youth_Read-hexor.log 2>&1" >> $mergedListFile
     echo "*/30 5-23 * * * node /scripts/somescripts/baidu/baidu_speed.js >> //scripts/logs/baidu_speed.log 2>&1" >> $mergedListFile
 fi
