32e5f4fba88078354b72f9dfe2ede00fc70c7d8a
Angeliss <59443350+lion-goose@users.noreply.github.com>
2021-06-11 10:44:08 +0800

    Update shell_mod_script.sh

--- someShell/shell_mod_script.sh
+++ someShell/shell_mod_script.sh
@@ -10,11 +10,12 @@ echo "附加功能2，创建其他任务"
 if [ ! -d "/scripts/somescripts/" ]; then
     echo "任务文件不存在，创建任务文件夹"
     cp -r /data/somescripts/ /scripts/somescripts
-    echo "# 百度和中青任务" >> $mergedListFile
-    echo "*/30 5-23 * * * node /scripts/somescripts/youth/youth.js >> /scripts/logs/youth.log 2>&1" >> $mergedListFile
-    echo "15 5,8,11,14,17,20 * * * node /scripts/somescripts/youth/Youth_Read-ange.js >> /scripts/logs/Youth_Read-ange.log 2>&1" >> $mergedListFile
-    echo "45 6,9,12,15,18,21 * * * node /scripts/somescripts/youth/Youth_Read-hexor.js >> /scripts/logs/Youth_Read-hexor.log 2>&1" >> $mergedListFile
-    echo "*/30 5-23 * * * node /scripts/somescripts/baidu/baidu_speed.js >> //scripts/logs/baidu_speed.log 2>&1" >> $mergedListFile
  else
     echo "任务存在，跳过..."
 fi
+echo "创建其他定时任务"
+echo "# 百度和中青任务" >> $mergedListFile
+echo "*/30 5-23 * * * node /scripts/somescripts/youth/youth.js >> /scripts/logs/youth.log 2>&1" >> $mergedListFile
+echo "15 5,8,11,14,17,20 * * * node /scripts/somescripts/youth/Youth_Read-ange.js >> /scripts/logs/Youth_Read-ange.log 2>&1" >> $mergedListFile
+echo "45 6,9,12,15,18,21 * * * node /scripts/somescripts/youth/Youth_Read-hexor.js >> /scripts/logs/Youth_Read-hexor.log 2>&1" >> $mergedListFile
+echo "*/30 5-23 * * * node /scripts/somescripts/baidu/baidu_speed.js >> //scripts/logs/baidu_speed.log 2>&1" >> $mergedListFile
