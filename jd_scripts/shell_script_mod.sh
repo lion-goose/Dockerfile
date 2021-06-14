d784a05756ea36d0995221a0a817b8068fff7930
Angeliss <59443350+lion-goose@users.noreply.github.com>
2021-06-14 23:44:38 +0800

    Update shell_script_mod.sh

--- jd_scripts/shell_script_mod.sh
+++ jd_scripts/shell_script_mod.sh
@@ -1,20 +1,19 @@
 #backup仓库脚本
-function initLiongoose() {
-    git clone https://github.com/lion-goose/BackUp.git /lion-goose
+function initJDHelloWorld() {
+    git clone https://github.com/JDHelloWorld/jd_scripts.git /JDHelloWorld
 }
 
-function LiongooseBackup() {
-    if [ ! -d "/lion-goose/" ]; then
-        echo "未检查到lion-goose仓库脚本，初始化下载相关脚本"
-        initLiongoose
+function JDHelloWorld() {
+    if [ ! -d "/JDHelloWorld/" ]; then
+        echo "未检查到JDHelloWorld仓库脚本，初始化下载相关脚本"
+        initJDHelloWorld
     else
-        echo "更新lion-goose脚本相关文件"
-        git -C /lion-goose reset --hard
-        git -C /lion-goose pull --rebase
+        echo "更新JDHelloWorld脚本相关文件"
+        git -C /JDHelloWorld reset --hard
+        git -C /JDHelloWorld pull --rebase
     fi
     ##复制文件
-    cp -f /lion-goose/jd*.js /scripts/
-    echo "0,1 0 * * * node /scripts/jd_jxcfdtx.js |ts >> /scripts/logs/jd_jxcfdtx.log 2>&1" >> /scripts/docker/merged_list_file.sh
+    cp -f /JDHelloWorld/jd*.js /scripts/
     
 }
-LiongooseBackup
+JDHelloWorld
