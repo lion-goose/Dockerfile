5c81971bddba3ea614b637b1e4183d69a90bf808
Angeliss <59443350+lion-goose@users.noreply.github.com>
2021-11-12 11:11:37 +0800

    Update shell_mod_script.sh

--- someShell/shell_mod_script.sh
+++ someShell/shell_mod_script.sh
@@ -158,8 +158,6 @@ echo "#E5AutoApi调用任务 " >>$mergedListFile
 echo "25 */6 * * * cd /data/somescripts/AutoApiSecret && sh start.sh" >>$mergedListFile
 echo "#京东饭粒" >>$mergedListFile
 echo "24 1,15,23 * * * node /scripts/jd_fanli.js >> /scripts/logs/jd_fanli.log 2>&1" >>$mergedListFile
-echo "#星店长" >>$mergedListFile
-echo "39 10,22 * * * node /scripts/jd_1111xdz.js >> /scripts/logs/jd_1111xdz.log 2>&1" >>$mergedListFile
 echo "#美团限时红包" >>$mergedListFile
 echo "0 11,14,17,21,0,1,2,3 * * * cd /data/custom_scripts && python3 meituan.py |ts >>/data/logs/meituan.log 2>&1 &" >>$mergedListFile
 echo "#seresCheckin任务" >>$mergedListFile
