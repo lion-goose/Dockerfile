fb6469c2a07525940aab9dbb6f28e90661cf5d09
Angeliss <59443350+lion-goose@users.noreply.github.com>
2021-11-29 14:57:27 +0800

    Update docker_entrypoint.sh

--- jd_scripts/docker_entrypoint.sh
+++ jd_scripts/docker_entrypoint.sh
@@ -5,7 +5,7 @@ set -e
 if [ $1 ]; then
     run_cmd=$1
 fi
-[ -f /scripts/package.json ] && before_package_json=$(cat /scripts/package.json)
+# [ -f /scripts/package.json ] && before_package_json=$(cat /scripts/package.json)
 if [ -f "/scripts/logs/pull.lock" ]; then
   echo "存在更新锁定文件，跳过git pull操作..."
 else
@@ -19,13 +19,13 @@ else
   if [ ! -d /scripts/node_modules ]; then
     echo "容器首次启动，执行npm install..."
     npm install --loglevel error --prefix /scripts
-  else
-    if [[ "${before_package_json}" != "$(cat /scripts/package.json)" ]]; then
-      echo "package.json有更新，执行npm install..."
-      npm install --loglevel error --prefix /scripts
-    else
-      echo "package.json无变化，跳过npm install..."
-    fi
+#   else
+#     if [[ "${before_package_json}" != "$(cat /scripts/package.json)" ]]; then
+#       echo "package.json有更新，执行npm install..."
+#       npm install --loglevel error --prefix /scripts
+#     else
+#       echo "package.json无变化，跳过npm install..."
+#     fi
   fi
 fi
 
@@ -37,9 +37,13 @@ echo "------------------------------------------------执行定时任务任务sh
 sh /jds/jd_scripts/task_shell_script.sh
 echo "--------------------------------------------------默认定时任务执行完成---------------------------------------------------"
 
-echo "package.json有更新，执行npm install..."
-npm install --loglevel error --prefix /scripts
-
+[ -f /scripts/package.json ] && before_package_json=$(cat /scripts/package.json)
+if [[ "${before_package_json}" != "$(cat /scripts/package.json)" ]]; then
+  echo "package.json有更新，执行npm install..."
+  npm install --loglevel error --prefix /scripts
+else
+  echo "package.json无变化，跳过npm install..."
+fi
 
 if [ $run_cmd ]; then
     echo "Start crontab task main process..."
