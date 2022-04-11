163a4df5c7ad50fbac4ee8a227eb80cb99a3f3b4
Angeliss <59443350+lion-goose@users.noreply.github.com>
2022-04-11 17:20:58 +0800

    Create docker_install.sh

---
+++ someShell/docker_install.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+clear
+
+red='\033[0;31m'
+green='\033[0;32m'
+
+
+pre_check() {
+    [[ $EUID -ne 0 ]] && echo -e "${red}错误: ${plain} 需要root权限\n" && exit 1
+
+    command -v git >/dev/null 2>&1
+    if [[ $? != 0 ]]; then
+        echo -e "正在安装Git..."
+        apt install git -y >/dev/null 2>&1
+        echo -e "${green}Git${green} 安装成功"
+    fi
+
+    command -v curl >/dev/null 2>&1
+    if [[ $? != 0 ]]; then
+        echo -e "正在安装curl..."
+        apt install curl -y >/dev/null 2>&1
+        echo -e "${green}curl${green} 安装成功"
+    fi
+
+    command -v docker >/dev/null 2>&1
+    if [[ $? != 0 ]]; then
+        echo -e "正在安装Docker..."
+        bash <(curl -fsL https://get.docker.com) >/dev/null 2>&1
+        echo -e "${green}Docker${green} 安装成功"
+    fi
+
+    command -v tar >/dev/null 2>&1
+    if [[ $? != 0 ]]; then
+        echo -e "正在安装tar..."
+        apt install tar -y >/dev/null 2>&1
+        echo -e "${green}tar${green} 安装成功"
+    fi
+}
+
+pre_check
