8dd2de2dc65e968c1ca1b0734ee0240f8195173d
Angeliss <59443350+lion-goose@users.noreply.github.com>
2022-04-26 15:48:26 +0800

    Update docker_install.sh

--- someShell/docker_install.sh
+++ someShell/docker_install.sh
@@ -13,32 +13,28 @@ function pre_check() {
     if [[ $? != 0 ]]; then
         echo -e "正在安装Git..."
         apt install git -y >/dev/null 2>&1
-        echo -e "${green}Git${green} 安装成功"
+        echo -e "${green}Git${plain} 安装成功"
     fi
 
     command -v curl >/dev/null 2>&1
     if [[ $? != 0 ]]; then
         echo -e "正在安装curl..."
         apt install curl -y >/dev/null 2>&1
-        echo -e "${green}curl${green} 安装成功"
+        echo -e "${green}curl${plain} 安装成功"
     fi
 
     command -v docker >/dev/null 2>&1
     if [[ $? != 0 ]]; then
         echo -e "正在安装Docker..."
         bash <(curl -fsL https://get.docker.com) >/dev/null 2>&1
-        systemctl enable docker.service
-        systemctl enable containerd.service
-        groupadd docker
-        usermod -aG docker $USER
-        echo -e "${green}Docker${green} 安装成功"
+        echo -e "${green}Docker${plain} 安装成功"
     fi
 
     command -v tar >/dev/null 2>&1
     if [[ $? != 0 ]]; then
         echo -e "正在安装tar..."
         apt install tar -y >/dev/null 2>&1
-        echo -e "${green}tar${green} 安装成功"
+        echo -e "${green}tar${plain} 安装成功"
     fi
 }
 
